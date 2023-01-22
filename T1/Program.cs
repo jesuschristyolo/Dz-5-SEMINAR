15e1e1a921fe97a6eb8315af53dfcf2ffd5b27c7
Jesuschrist <valishev57@mail.ru>
2023-01-22 22:39:05 +0500

    complete 2 tasks

--- T1/Program.cs
+++ T1/Program.cs
@@ -1,2 +1,49 @@
-﻿using static System.Console;
+﻿//  Напишите программу реализующую методы формирования массива, заполненного случайными положительными 
+//трёхзначными числами, и подсчета количества чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+using static System.Console;
 Clear();
+
+
+Console.WriteLine("Input count of numbers in array:  ");
+int.TryParse(ReadLine(), out int numbers);
+int[] size = new int[numbers];
+
+Randomnumbers(size);
+PrintArray(size);
+CountArray(size);
+
+Write($"-> {CountArray(size)} четные");
+
+
+
+void Randomnumbers(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        num[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length - 1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    Write($"{inArray[inArray.Length - 1]}] ");
+}
+
+int CountArray(int[] num)
+{
+    int count = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        if (num[i] % 2 == 0)
+            count++;
+    }
+    return count;
+
+}
---
+++ T2/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу реализующую методы формирования массива, 
+// заполненного случайными числами и подсчета суммы элементов, 
+// стоящих на нечётных позициях.
+
+
+using static System.Console;
+Clear();
+
+Console.WriteLine("Input count of numbers in array:  ");
+int.TryParse(ReadLine(), out int numbers);
+int[] size = new int[numbers];
+
+Randomnumbers(size);
+PrintArray(size);
+SumCounts(size);
+
+Write($"сумма четных элементов массива равна: {SumCounts(size)} ");
+
+
+void Randomnumbers(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        num[i] = new Random().Next(0, 1000);
+    }
+}
+
+void PrintArray(int[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length - 1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    Write($"{inArray[inArray.Length - 1]}] ");
+}
+
+int SumCounts(int[] size)
+{
+    int sum = 0;
+    for (int i = 0; i < size.Length; i += 2)
+    {
+        sum = sum + size[i];
+    }
+    return sum;
+}
